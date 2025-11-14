dd5883432a69cd2705bfce6acf0b56ee8922ea6d
Saurjyadipta Majumdar <iamsaurjyadipta@gmail.com>
2025-11-14 20:46:23 +0000

    not working

--- next.config.ts
+++ next.config.ts
@@ -23,17 +23,16 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
